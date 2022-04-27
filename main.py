1775588555c1fa812d14928c57e1bebe841c7d16
RyanXSCH <104444321+RyanXSCH@users.noreply.github.com>
2022-04-27 12:39:31 +0700

    Test.py

--- main.py
+++ main.py
@@ -1,170 +1,52 @@
-import ssl
-import time
-import queue
-import threading
-from random import randint, choice
-from urllib3.exceptions import InsecureRequestWarning
-from urllib.parse import urlparse
-from http import cookiejar
+import random, string, requests, time
+from colorama import Fore
+from discord_webhook import DiscordWebhook
+import os
+os.system("cls" or "clear")
+print(Fore.RED+"""
 
-import requests
-from pystyle import Colorate, Colors, Write, Add, Center
+Discord Server : https://discordapp.com/api/webhooks/805456884252540960/ds7Q881gLCo5MOBJbH6M-K9sgkhiUIAz1IKVwGvvTblr327FG9WPLAtR91ppWx-hLVQ5 
+        My discord : @K4Sp3r#1337
 
-from Data.UserAgent import UserAgent
-from Data.Lists import DeviceTypes, Platforms, Channel, ApiDomain
-from utils import *
+ _________  _   __      _________       __              ______  __                    __                     
+|  _   _  |(_) [  |  _ |  _   _  |     [  |  _        .' ___  |[  |                  [  |  _                 
+|_/ | | \_|__   | | / ]|_/ | | \_|.--.  | | / ]      / .'   \_| | |--.  .---.  .---.  | | / ] .---.  _ .--.  
+    | |   [  |  | '' <     | |  / .'`\ \| '' <       | |        | .-. |/ /__\\/ /'`\] | '' < / /__\\[ `/'`\] 
+   _| |_   | |  | |`\ \   _| |_ | \__. || |`\ \      \ `.___.'\ | | | || \__.,| \__.  | |`\ \| \__., | |     
+  |_____| [___][__|  \_] |_____| '.__.'[__|  \_]      `.____ .'[___]|__]'.__.''.___.'[__|  \_]'.__.'[___]    
+                                                                                                             
 
-class BlockCookies(cookiejar.CookiePolicy):
-    return_ok = set_ok = domain_return_ok = path_return_ok = lambda self, *args, **kwargs: False
-    netscape = True
-    rfc2965 = hide_cookie2 = False
-
-requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
-ssl._create_default_https_context = ssl._create_unverified_context
-r                                 = requests.Session()
-countQueue                        = queue.Queue()
-sentRequests                      = 0
-completed                         = False
-
-r.cookies.set_policy(BlockCookies())
-def Banner():
-    clearConsole()
-    Banner1 = r"""
-╔╦╗  ╦  ╦╔═  ╔═╗  ╦ ╦  ╔═╗  ╦═╗  ╔═╗
- ║   ║  ╠╩╗  ╚═╗  ╠═╣  ╠═╣  ╠╦╝  ║╣ 
- ╩   ╩  ╩ ╩  ╚═╝  ╩ ╩  ╩ ╩  ╩╚═  ╚═╝
-        discord.gg/devcenter
-"""
-
-    Banner2 = r"""
-  ,           ,
- /             \
-((__-^^-,-^^-__))
- `-_---' `---_-'
-  <__|o` 'o|__>
-     \  `  /
-      ): :(
-      :o_o:
-       "-" 
-       """
-
-    print(Center.XCenter(Colorate.Vertical(Colors.yellow_to_red, Add.Add(Banner2, Banner1, center=True), 2)))
-
-def sendView():
-    proxy         = {f'{proxyType}': f'{proxyType}://{choice(proxyList)}'}
-    platform      = choice(Platforms)
-    osVersion     = randint(1, 12)
-    DeviceType    = choice(DeviceTypes)
-    headers       = {
-                        "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
-                        "user-agent": choice(UserAgent)
-                    }
-    appName       = choice(["tiktok_web", "musically_go"])
-    Device_ID     = randint(1000000000000000000, 9999999999999999999)
-    apiDomain     = choice(ApiDomain)
-    channelLol    = choice(Channel)
-    URI           = f"https://{apiDomain}/aweme/v1/aweme/stats/?channel={channelLol}&device_type={DeviceType}&device_id={Device_ID}&os_version={osVersion}&version_code=220400&app_name={appName}&device_platform={platform}&aid=1988"
-    data          = f"item_id={itemID}&play_delta=1"
-
+    Made By : K4Sp3r
+     Insta : @K4S 
+      Snap : Fuun
+"""+Fore.WHITE)
+time.sleep(.5)
+hmletters = int(input('how many letters you want to check : '))
+print("")
+wbhook = input("Webhook url : ")
+print()
+time.sleep(.3)
+while True:
     try:
-        req = r.post(URI, headers=headers, data=data, proxies=proxy, timeout=5, verify=False)
-        return True
+        usernames = ('').join(random.choices(string.ascii_lowercase + string.digits, k=hmletters))
+        webhook = DiscordWebhook(url=wbhook, content=f'`{usernames}` | Might be Available|')
+        r = requests.get(f'https://m.tiktok.com/node/share/user/@{usernames}')
+        responsecode = r.json()['statusCode']
+        
+        if responsecode == 10202:
+            print(Fore.CYAN + "[+] " + Fore.GREEN + "Available" + Fore.WHITE + ' |=>' + Fore.LIGHTMAGENTA_EX + f' {usernames}'+Fore.WHITE+" <=|" + Fore.CYAN + " [+]")
+            f = open("availables.txt", "a", encoding='utf-8')
+            f.write(f"{usernames} | Might be Available or Banned|\n")
+            webhook.execute()
+        elif responsecode == 10222:
+           print(Fore.CYAN+"[-] "+Fore.RED + "UnAvailable"+ Fore.WHITE +' |=>'+Fore.YELLOW+ f' {usernames}'+Fore.WHITE+" <=|"+Fore.CYAN+" [-]")
+        elif responsecode == 0:
+            print(Fore.CYAN+"[-] "+Fore.RED + "UnAvailable"+ Fore.WHITE +' |=>'+Fore.YELLOW+ f' {usernames}'+Fore.WHITE+" <=|"+Fore.CYAN+" [-]")
+        elif responsecode == 10221:
+            print(Fore.CYAN+"[-] "+Fore.RED + "Banned"+ Fore.WHITE +' |=>'+Fore.YELLOW+ f' {usernames}'+Fore.WHITE+" <=|"+Fore.CYAN+" [-]")
+        elif responsecode == 10223:
+            print(Fore.CYAN+"[-] "+Fore.RED + "Banned"+ Fore.WHITE +' |=>'+Fore.YELLOW+ f' {usernames}'+Fore.WHITE+" <=|"+Fore.CYAN+" [-]")
+        else:
+            print(Fore.CYAN+"[X] "+Fore.WHITE+f"i cant find {usernames}'s statusCode !!' "+Fore.CYAN+" [X]"+Fore.WHITE)
     except:
-        return False
-
-def sendShare():
-    platform = choice(Platforms)
-    osVersion = randint(1, 12)
-    DeviceType = choice(DeviceTypes)
-    headers = {
-        "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
-        "user-agent": choice(UserAgent)
-    }
-    appName = choice(["tiktok_web", "musically_go"])
-    Device_ID = randint(1000000000000000000, 9999999999999999999)
-    apiDomain = choice(ApiDomain)
-    channelLol = choice(Channel)
-    URI = f"https://{apiDomain}/aweme/v1/aweme/stats/?channel={channelLol}&device_type={DeviceType}&device_id={Device_ID}&os_version={osVersion}&version_code=220400&app_name={appName}&device_platform={platform}&aid=1988"
-    data = f"item_id={itemID}&share_delta=1"
-
-    try:
-        req = r.post(URI, headers=headers, data=data, verify=False)
-        return True
-    except:
-        return False
-
-def clearURL(link):
-    parsedURL = urlparse(link)
-    host = parsedURL.hostname.lower()
-    if "vm.tiktok.com" == host or "vt.tiktok.com" == host:
-        UrlParsed = urlparse(r.head(link, verify=False, allow_redirects=True, timeout=5).url)
-        return UrlParsed.path.split("/")[3]
-    else:
-        UrlParsed = urlparse(link)
-        return UrlParsed.path.split("/")[3]
-
-def proccessThread(sendProccess):
-    while not completed:
-        if sendProccess():
-            countQueue.put(1)
-
-def countThread():
-    global sentRequests, completed
-    while True:
-        countQueue.get()
-        sentRequests += 1
-        if amount > 0:
-            if sentRequests >= amount:
-                completed = True
-
-def progressThread():
-    while True:
-        start = time.time()
-        startReq = sentRequests
-        time.sleep(1)
-        end = time.time()
-        endReq = sentRequests
-
-        elapsed = end - start
-        elapsedReq = endReq - startReq
-
-        print(f"{sentRequests} sent requests! {elapsedReq} requests/second.", end="\r")
-
-if (__name__ == "__main__"):
-    clearConsole(); Banner()
-    VideoURI     = str(Write.Input("Video Link > ", Colors.yellow_to_red, interval=0.0001))
-    amount       = int(Write.Input("Amount (0=inf) > ", Colors.yellow_to_red, interval=0.0001))
-    nThreads     = int(Write.Input("Thread Amount > ", Colors.yellow_to_red, interval=0.0001)); clearConsole(); Banner()
-    sendType     = int(Write.Input("[0] - Views\n[1] - Shares > ", Colors.yellow_to_red, interval=0.0001)); clearConsole(); Banner()
-    itemID       = clearURL(VideoURI)
-    proxyChoose  = True
-    while proxyChoose:
-        proxyType = Write.Input("Select proxy type:\n[0] - http\n[1] - socks4\n[2] - socks5 > ", Colors.yellow_to_red, interval=0.0001)
-        if proxyType == "0":
-            proxyType = "http"
-            proxyChoose = False
-        elif proxyType == "1":
-            proxyType = "socks4"
-            proxyChoose = False
-        elif proxyType == "2":
-            proxyType = "socks5"
-            proxyChoose = False
-
-    proxyList = readProxiesFile()
-    clearConsole(); Banner()
-
-    print(Colorate.Horizontal(Colors.yellow_to_red, f"Hits are not counted"))
-    print(Colorate.Horizontal(Colors.yellow_to_red, f"Bot started! Check your video stats in 5 minutes !"))
-
-    if sendType == 0:
-        sendProcess = sendView
-    elif sendType == 1:
-        sendProcess = sendShare
-    else:
-        print(f"Error {sendType}")
-
-    threading.Thread(target=countThread).start()
-    threading.Thread(target=progressThread).start()
-
-    for n in range(nThreads):
-        threading.Thread(target=proccessThread, args=(sendProcess,)).start()
+        pass
